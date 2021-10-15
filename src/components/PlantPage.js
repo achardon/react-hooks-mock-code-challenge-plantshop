bdb5e08423ac9316ca88a33aa00619c2e69e7a07
Alessandra Chardon <achardon@ucla.edu>
2021-10-15 13:48:31 -0400

    completed first two core deliverables

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+
+  console.log(plants)
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(r => r.json())
+    .then(data => {
+      setPlants((currentPlants) => data)
+    })
+  }, [])
+
+  function onAddPlant(newPlant) {
+    fetch('http://localhost:6001/plants', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newPlant)
+    })
+    .then(r => r.json())
+    .then(data => {
+      setPlants((currentPlant) => [...plants, {
+        ...data, id: data.id
+      }])
+    })
+  }
+
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm onAddPlant={onAddPlant}/>
       <Search />
-      <PlantList />
+      <PlantList
+      plants={plants}
+      />
     </main>
   );
 }
